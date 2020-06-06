fec0821d127ba92514db00f7e07e1380652e5844
Anthony David Cobb <anthonydcobb@gmail.com>
2020-06-06 16:28:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function moveDodgerLeft() {
     let leftNumbers = dodger.style.left.replace('px', '')
     let left = parseInt(leftNumbers, 10)
     if (left > 0) {
-    dodger.style.left = `${left - 1}px`
+    dodger.style.left = `${left - 5}px`
   }
 }
 
@@ -15,7 +15,7 @@ function moveDodgerRight() {
   let leftNumbers = dodger.style.left.replace('px', '')
   let left = parseInt(leftNumbers, 10)
   if(left < 360) {
-    dodger.style.left = `${left + 1}px`
+    dodger.style.left = `${left + 5}px`
   }
 }
 
