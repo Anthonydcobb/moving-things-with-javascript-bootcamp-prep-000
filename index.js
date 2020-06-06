dba7c0d581a4ae7cdab87fbf6999248acfa4aae9
Anthony David Cobb <anthonydcobb@gmail.com>
2020-06-06 16:27:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function moveDodgerRight() {
   let leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
     dodger.style.left = `${left + 1}px`
+    console.log(`${left}`)
 
 }
 
