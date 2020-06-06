98a2001e6f8ed2e1d2ea07218b19ad2e05d07cfa
Anthony David Cobb <anthonydcobb@gmail.com>
2020-06-06 16:26:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const app = "I don't do much."
 
-var dodger = document.getElementById('dodger')
+const dodger = document.getElementById('dodger')
 dodger.style.backgroundColor = '#FF69B4'
 
 function moveDodgerLeft() {
-    var leftNumbers = dodger.style.left.replace('px', '')
-    var left = parseInt(leftNumbers, 10)
+    let leftNumbers = dodger.style.left.replace('px', '')
+    let left = parseInt(leftNumbers, 10)
     if (left > 0) {
     dodger.style.left = `${left - 1}px`
   }
 }
 
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(rightNumbers, 10)
-    dodger.style.right = `${right + 1}px`
+  let leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+    dodger.style.left = `${left + 1}px`
 
 }
 
