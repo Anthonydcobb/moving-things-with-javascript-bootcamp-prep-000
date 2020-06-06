4d93c8dcb4797fd0b244ed7b221bd79bf3ec7a0f
Anthony David Cobb <anthonydcobb@gmail.com>
2020-06-06 16:22:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ function moveDodgerLeft() {
   }
 }
 
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+  if (right < 300) {
+    dodger.style.right = `${right + 1}px`
+  } 
+}
+
 
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
