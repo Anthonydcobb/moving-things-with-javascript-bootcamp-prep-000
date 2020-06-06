9932ca356a15631f1db164416ae6d86076555762
Anthony David Cobb <anthonydcobb@gmail.com>
2020-06-06 16:15:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ var dodger = document.getElementById('dodger')
 dodger.style.backgroundColor = '#FF69B4'
 
 document.addEventListener('keydown', function(e) {
- if(e.which === 37) {
-
- }
+  if (e.which === 37) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
 })
