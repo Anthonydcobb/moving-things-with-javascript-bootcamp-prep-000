ab24e2e6879a449bc6dc25d12373b32181422240
Anthony David Cobb <anthonydcobb@gmail.com>
2020-06-06 15:56:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = "I don't do much."
 
 var dodger = document.getElementById('dodger')
+dodger.style.backgroundColor = '#FF69B4'
 
 document.addEventListener('keydown', function(e) {
  console.log(e.which)
